c81c6a030d9dc47cd31d18de4977f42262d92d46
NastiaKyrakina <37176237+NastiaKyrakina@users.noreply.github.com>
2019-04-15 12:32:33 +0300

    work two

---
+++ gulpfile.js
@@ -0,0 +1,48 @@
+const gulp = require('gulp');
+const pug = require('gulp-pug');
+const sass = require('gulp-sass');
+const browserSync = require('browser-sync').create();
+const autoprefixer = require('gulp-autoprefixer');
+
+const devFolder = './dev';
+const buildFolder = './dest';
+const srcPugs = [`${devFolder}/views/index.pug`, `${devFolder}/views/blog.pug`];
+const destHTML = [`${buildFolder}/index.html`,`${buildFolder}/blog.html`];
+
+var imagemin    = require('gulp-imagemin');
+var pngquant    = require('imagemin-pngquant');
+
+browserSync.init({
+  server: {
+    baseDir: './dest'
+  }
+});
+
+gulp.task('views', function buildHTML() {
+  return gulp.src(srcPugs)
+  .pipe(pug())
+  .pipe(gulp.dest(`${buildFolder}`))
+});
+
+gulp.task('styles', function buildStyles() {
+  return gulp.src(`${devFolder}/styles/main.scss`)
+    .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer())
+    .pipe(gulp.dest(`${buildFolder}/css`));
+});
+
+gulp.task('img', function () {
+  return gulp.src(`${devFolder}/img/*`)
+      .pipe(imagemin({
+        interlaced: true,
+        progressive: true,
+        svgoPlugins: [{removeViewBox: false}],
+        use: [pngquant()]
+      }))
+      .pipe(gulp.dest(`${buildFolder}/img`));
+});
+
+gulp.task('watch', function () {
+  gulp.watch([`${devFolder}/styles/*.scss`, `${devFolder}/views/**/*.pug`, ], ['styles', 'views']);
+  gulp.watch(destHTML).on('change', browserSync.reload);
+});
